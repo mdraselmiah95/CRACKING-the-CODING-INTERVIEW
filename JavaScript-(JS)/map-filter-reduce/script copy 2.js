6f9607a0919a264d5f3a9fa0a0500814861d5fdb
mdraselmiah95 <russelroy15@gmail.com>
2023-04-25 01:37:21 +0600

    Map vs forEach

---
+++ JavaScript-(JS)/map-filter-reduce/script copy 2.js
@@ -0,0 +1,58 @@
+// map filter and reduce
+
+// Polyfill for Map()
+
+//Array.map((num,i,arr)=>{})
+
+Array.prototype.newMap = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    temp.push(cb(this[i], i, this));
+  }
+  return temp;
+};
+
+const nums = [1, 2, 4, 5, 7, 8, 9];
+
+const multiply = nums.newMap((num, i, arr) => {
+  return num * 3 + i;
+});
+
+// console.log(multiply);
+
+// Polyfill for Filter()
+
+Array.prototype.newFilter = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      temp.push(this[i]);
+    }
+  }
+  return temp;
+};
+
+const biggerNum = nums.newFilter((num) => {
+  return num > 3;
+});
+
+// console.log(biggerNum);
+
+// Polyfill for Reduce()
+
+//arr.reduce((acc,curr,i,arr)=>{},initialValue)
+
+Array.prototype.newReduce = function (cb, initialValue) {
+  let accumulator = initialValue;
+
+  for (let i = 0; i < this.length; i++) {
+    accumulator = accumulator ? cb(accumulator, this[i], i, this) : this[i];
+  }
+  return accumulator;
+};
+
+const sum = nums.newReduce((acc, curr, i, arr) => {
+  return acc + curr;
+}, 0);
+
+// console.log(sum);
--- JavaScript-(JS)/map-filter-reduce/script.js
+++ JavaScript-(JS)/map-filter-reduce/script.js
@@ -1,60 +1,15 @@
 // map filter and reduce
 
-// Polyfill for Map()
+// Difference between map vs forEach ?
 
-//Array.map((num,i,arr)=>{})
+const arr = [2, 5, 6, 4, 7, 3];
 
-Array.prototype.newMap = function (cb) {
-  let temp = [];
-  for (let i = 0; i < this.length; i++) {
-    temp.push(cb(this[i], i, this));
-  }
-  return temp;
-};
-
-const nums = [1, 2, 4, 5, 7, 8, 9];
-
-const multiply = nums.newMap((num, i, arr) => {
-  return num * 3 + i;
+const mapResult = arr.map((arr) => {
+  return arr + 2;
 });
 
-// console.log(multiply);
-
-// Polyfill for Filter()
-
-Array.prototype.newFilter = function (cb) {
-  let temp = [];
-  for (let i = 0; i < this.length; i++) {
-    if (cb(this[i], i, this)) {
-      temp.push(this[i]);
-    }
-  }
-  return temp;
-};
-
-const biggerNum = nums.newFilter((num) => {
-  return num > 3;
+const forEachResult = arr.forEach((arr) => {
+  return arr + 2;
 });
 
-// console.log(biggerNum);
-
-// Polyfill for Reduce()
-
-//arr.reduce((acc,curr,i,arr)=>{},initialValue)
-
-Array.prototype.newReduce = function (cb, initialValue) {
-  let accumulator = initialValue;
-
-  for (let i = 0; i < this.length; i++) {
-    accumulator = accumulator ? cb(accumulator, this[i], i, this) : this[i];
-  }
-  return accumulator;
-};
-
-const sum = nums.newReduce((acc, curr, i, arr) => {
-  return acc + curr;
-}, 0);
-
-// console.log(sum);
-
-// Difference between map vs forEach ? 15:51
+console.log(mapResult, forEachResult);
