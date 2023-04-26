d9af455aa560a134a3040ad2920d5afd293692db
mdraselmiah95 <russelroy15@gmail.com>
2023-04-27 04:06:54 +0600

    Arrays - Data Structures and Algorithms in Javascript

--- JavaScript-(JS)/Arrays/script.js
+++ JavaScript-(JS)/Arrays/script.js
@@ -16,5 +16,40 @@ const names = ["rasel", "shakib", "shohan", "noman", "kazol", "rohan"];
 
 for (let i = 0; i < names.length; i++) {
   const element = names[i];
-  console.log(element);
+  //   console.log(element);
 }
+
+const newName = names.map((name, i, arr) => {
+  return name + i;
+});
+
+// console.log(newName);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+//map in JavaScript
+const newNum = numbers.map((num, i, arr) => {
+  return num + 2;
+});
+
+const refileNum = numbers.filter((num) => {
+  return num > 2;
+});
+
+const sum = numbers.reduce((prev, curr) => {
+  return prev + curr;
+}, 0);
+
+const someNum = numbers.some((item) => {
+  return item > 2;
+});
+
+const everyNum = numbers.every((item) => {
+  return item > 3;
+});
+
+const findNum = numbers.find((num) => {
+  return num > 4;
+});
+
+console.log(findNum);
