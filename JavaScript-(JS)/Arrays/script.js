be36fb3e8478e79d6e25313c42e26c3022b48f9c
mdraselmiah95 <russelroy15@gmail.com>
2023-04-27 03:49:27 +0600

    Arrays - Data Structures and Algorithms in Javascript

--- JavaScript-(JS)/Arrays/script.js
+++ JavaScript-(JS)/Arrays/script.js
@@ -1,1 +1,20 @@
-console.log(99);
+// Array Deceleration
+
+let person = {
+  name: "rasel",
+  age: 26,
+};
+
+let arr = ["apple", "banana", "mango", person];
+
+arr.push("orange");
+arr.pop();
+
+// console.log(arr);
+
+const names = ["rasel", "shakib", "shohan", "noman", "kazol", "rohan"];
+
+for (let i = 0; i < names.length; i++) {
+  const element = names[i];
+  console.log(element);
+}
