5f77af0e2a913eda2cf99239fa2197034c6aa7b0
mdraselmiah95 <russelroy15@gmail.com>
2023-05-14 00:08:44 +0600

    javascript method

--- JavaScript-(JS)/Functions/script.js
+++ JavaScript-(JS)/Functions/script.js
@@ -1,78 +1,21 @@
-// Function
+// Function vs Method
 
-// It is simply a piece of code which we can use in our program many time
+// Method=> it is nothing but object property holding
+// Function as "Value"
 
-// It is just like a variable holding some piece of code
-
-function nameOfFunction() {
-  console.log("Running the function");
-}
-nameOfFunction();
-
-let fun = function () {
-  // also called anonymous function
-  console.log("This is example of function expression");
+const myObject = {
+  name: "John Doe",
+  age: 30,
+  sayHello: function () {
+    alert("Hello, my name is " + this.name);
+  },
 };
 
-// Calling function expression
-// fun();
-
-let invitation = function (name = "defaultName", time = "night") {
-  console.log(`Welcome ${name} You are invited on our event ${time}`);
+let person = {
+  ageCalculate: function (birthYear = 2000) {
+    let age = 2023 - birthYear;
+    return age;
+  },
 };
 
-// invitation("Rasel", "evening");
-// invitation();
-
-// Retuning Values(How to return value from the function)
-
-let ageCalculation = function (birthYear, currentYear = 2023) {
-  let age = currentYear - birthYear;
-  return age;
-};
-
-const result = ageCalculation(1995);
-// console.log(result);
-
-// Arrow Function (ES6)
-
-const arrowFunc = (name) => `This is my ${name}`;
-
-// Passing function as an argument (Higher order function)
-
-let upperCase = function (str) {
-  return str.toUpperCase();
-};
-
-let lowerCase = function (str) {
-  return str.toLowerCase();
-};
-
-let transform = function (str, fun) {
-  return fun(str);
-};
-
-const result1 = transform("this is Rasel mia", upperCase);
-const result2 = transform("this is Rasel mia", lowerCase);
-// console.log({ result1, result2 });
-
-// Function returning another function
-
-let compliment = function (msg) {
-  return function (name) {
-    console.log(`${msg} ${name}`);
-  };
-};
-
-// compliment("I am good Programer")("rasel");
-
-let complimented = compliment("You are a good coder");
-
-// complimented("Rasel");
-
-// Immediately invoked function Expression  IIFE
-// Executed Only Once
-
-(function (name) {
-  console.log("This function will never executed again", name);
-})("IIFE");
+console.log(`Current age is ${person.ageCalculate(1995)}`);
---
+++ JavaScript-(JS)/Learn-javascript/object.js
@@ -0,0 +1,18 @@
+// Introduction to Object in JavaScript 👩🏻‍💻
+
+let car = {
+  color: "block",
+  model: "2022",
+  company: "Honda",
+};
+// console.log(car.color);
+
+// Accessing the JavaScript Object properties
+
+// let obj = new Object();
+
+// Modify the Object
+
+car.color = "Gray";
+delete car.color;
+// console.log(car);
