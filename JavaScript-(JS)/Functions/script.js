ff843ac661f48da65081305c5cd1bd6c427344f3
mdraselmiah95 <russelroy15@gmail.com>
2023-05-14 20:33:14 +0600

    Javascript array object

--- JavaScript-(JS)/Functions/script.js
+++ JavaScript-(JS)/Functions/script.js
@@ -18,4 +18,53 @@ let person = {
   },
 };
 
-console.log(`Current age is ${person.ageCalculate(1995)}`);
+// console.log(`Current age is ${person.ageCalculate(1995)}`);
+
+// THis keyword
+
+// in Each Method we have an access of special keyword called "this"
+
+let Doctor = {
+  firstName: "Rasel",
+  lastName: "Mia",
+  city: "Dhaka",
+  birthYear: 1995,
+  Education: "Software Engineer",
+  getSummery: function () {
+    // return `${this.firstName} ${this.lastName} lives in ${this.city} and born in ${this.birthYear} by education is ${this.Education} `;
+    return this;
+  },
+};
+
+// console.log(Doctor.getSummery());
+
+// forEach method of Array
+
+let dishes = [
+  "Pizza ",
+  "Pizza ",
+  "Paella ",
+  "Pierogi",
+  "Moussaka",
+  "biryani",
+  "naan",
+  "butter chicken",
+];
+
+for (let i = 0; i < dishes.length; i++) {
+  // console.log(dishes[i]);
+}
+
+// dishes.forEach((element) => console.log(element));
+
+const cars = [
+  { make: "Toyota", model: "Camry", year: 2019 },
+  { make: "Honda", model: "Civic", year: 2020 },
+  { make: "Ford", model: "Mustang", year: 2021 },
+];
+
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i].make);
+}
+
+cars.forEach((car) => console.log(car));
