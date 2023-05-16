d9ea9c98ca8ad93f7a2b1dbadd932f57055de05d
mdraselmiah95 <russelroy15@gmail.com>
2023-05-16 23:39:11 +0600

    new leadcode problem added

---
+++ JavaScript-(JS)/Functions/script copy 3.js
@@ -0,0 +1,51 @@
+"use strict";
+// Function
+// call and apply method
+// we can manually set the value of "this" keyword using "call and apply"
+
+let mainPlain = {
+  airline: "Air India",
+  iataCode: "F4",
+  booking: [],
+  book: function (flightName, name) {
+    console.log(
+      `${name} Booked Flight on ${this.airline} with flight Number ${this.iataCode} ${flightName}`
+    );
+    this.booking.push({
+      flight: `${this.airline}`,
+      name: name,
+      flightNumber: `${this.iataCode} ${flightName}`,
+    });
+  },
+};
+
+mainPlain.book(530, "Rasel");
+
+mainPlain.book(652, "Ibn Intiaz");
+console.log(mainPlain);
+
+let childPlain = {
+  airline: "Child Plain",
+  iataCode: "CP",
+  booking: [],
+};
+
+let book = mainPlain.book;
+book.call(childPlain, 569, "Jack");
+
+book.call(mainPlain, 699, "Rose");
+
+book.apply(childPlain, [510, "Tom Hank"]);
+console.log(childPlain);
+
+function greet() {
+  console.log(`Welcome ${this.firstName} ${this.lastName} on Coders World.`);
+}
+
+let user = {
+  firstName: "Tom",
+  lastName: "Cruise",
+};
+
+let greets = greet.bind(user);
+greets();
