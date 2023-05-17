96bbcc1678d9fb1f82e77565f4fd988372130a0f
mdraselmiah95 <russelroy15@gmail.com>
2023-05-18 00:49:40 +0600

    new code added

--- LeetCode/TEST.js
+++ LeetCode/TEST.js
@@ -8,4 +8,16 @@ function simpleArraySum(ar) {
 }
 
 const re = simpleArraySum([1, 2, 3, 4, 10, 11]);
-console.log(re);
+// console.log(re);
+
+const revInt = (num) => {
+  let myRevNum = num.toString().split("").reverse().join("");
+  if (myRevNum.endsWith("-")) {
+    myRevNum = "-" + myRevNum;
+    return parseInt(myRevNum);
+  } else {
+    return parseInt(myRevNum);
+  }
+};
+
+console.log(revInt(-123));
