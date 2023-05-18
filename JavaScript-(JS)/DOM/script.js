83fc689c0a01ef5ff9c3f005c95f678b82eadb1e
mdraselmiah95 <russelroy15@gmail.com>
2023-05-19 00:18:18 +0600

    new code added

---
+++ JavaScript-(JS)/DOM/script.js
@@ -0,0 +1,8 @@
+// Javascript DOM
+
+let para = document.querySelector("p");
+let headerQ = document.querySelector(".heading");
+let header = document.getElementsByClassName("heading");
+let headerID = document.getElementById("headerID");
+
+console.log(headerID);
