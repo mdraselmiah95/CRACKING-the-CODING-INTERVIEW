820cf067eee51e912fbc0f67d9376e96eeb60e5b
mdraselmiah95 <russelroy15@gmail.com>
2023-05-23 21:07:48 +0600

    new code added

--- JavaScript-(JS)/DOM/script.js
+++ JavaScript-(JS)/DOM/script.js
@@ -5,4 +5,18 @@ let headerQ = document.querySelector(".heading");
 let header = document.getElementsByClassName("heading");
 let headerID = document.getElementById("headerID");
 
-console.log(headerID);
+localStorage.setItem("passion", "coding");
+localStorage.setItem("age", 23);
+
+// Remove Item
+localStorage.removeItem("age");
+
+let vehicles = [
+  { company: "Honda", model: 2009 },
+  { company: "Toyota", model: 2012 },
+];
+
+let stringifyVehicle = JSON.stringify(vehicles);
+localStorage.setItem("vehicles", stringifyVehicle);
+
+console.log(localStorage);
