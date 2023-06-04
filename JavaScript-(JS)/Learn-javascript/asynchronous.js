d170dfc22340cbcde84ffae6be7b1d627e85bea0
mdraselmiah95 <russelroy15@gmail.com>
2023-06-04 16:57:00 +0600

    Learning JS

--- JavaScript-(JS)/Learn-javascript/asynchronous.js
+++ JavaScript-(JS)/Learn-javascript/asynchronous.js
@@ -9,7 +9,7 @@ setTimeout(() => {
 
 let getSomething = () => {
   return new Promise((resolve, reject) => {
-    // resolve("Some Data");
+    resolve("Some Data");
     reject("Some Error");
   });
 };
@@ -36,3 +36,48 @@ try {
 } catch (error) {
   console.log(error.message);
 }
+
+// Array Destructuring
+
+let hotel = {
+  name: "Helton",
+  location: "Cox Bazar",
+  categories: ["Bangla", "Italian", "English"],
+};
+
+// let [a, b, c] = hotel.categories;
+// console.log({ a, b, c });
+
+let items = [1, 2, 3, 4, 5];
+let [a, b, c] = items;
+// console.log(a, b, c);
+
+let [main, secondary] = hotel.categories;
+console.log(main, secondary);
+
+// let temp = main;
+// main = secondary;
+// secondary = temp;
+
+[secondary, main] = [main, secondary];
+
+// console.log(main, secondary);
+
+// Destructuring of Object
+
+let topHotel = {
+  name: "InterContinental",
+  Location: "24 street of Dhaka",
+  categories: ["Chinese", "Bangla", "Italian", "Thai", "Western"],
+  mainMenu: ["FoodA", "FoodB", "FoodC"],
+  openingHours: {
+    sunday: { open: "09:00AM", close: "11:00PM" },
+    monday: { open: "10:00AM", close: "12:00PM" },
+  },
+};
+
+// let { name: hotelName, Location: hotelLocation, mainMenu: menu } = topHotel;
+
+let { name, Menu = (newMenu = []), categories = [] } = topHotel;
+
+console.log({ newMenu });
